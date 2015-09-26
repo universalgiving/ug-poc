a3e4b07d63323e35ba225d7281d4050596dccd28
Zhenzhong Xu <flyfree@gmail.com>
2015-09-26 16:29:46 -0700

    add required 6 properties for volunteering

--- packages/devinitdata/devinitdata.js
+++ packages/devinitdata/devinitdata.js
@@ -23,16 +23,24 @@ if (Meteor.isServer) {
             var now = new Date().getTime();
 
             Volunteerings.insert({
+                    category: "Volunteer",
+                    thumbnail: "http://www.universalgiving.org/photo/image.do?id=3106",
                     title: "Volunteer in China",
-                    author: "Zhenzhong Xu",
+                    organization: "The GVN Foundation",
+                    length: "2-6 months",
+                    location: "China",
                     submitted: now,
                     description: "Great opportunity to volunteer in China!"
                 }
             );
 
             Volunteerings.insert({
+                    category: "Volunteer",
+                    thumbnail: "http://www.universalgiving.org/photo/image.do?id=5018",
                     title: "Rwanda Volunteer Program",
-                    author: "Zhenzhong Xu",
+                    organization: "WorldTeach",
+                    length: "2-12 weeks",
+                    location: "Rwanda",
                     submitted: now,
                     description: "The Global Volunteer Network currently has opportunities to work with children, and their community through our partner organization in Rwanda. Volunteers will assist our partner organisation's mission to facilitate processes to help eradicate. . ."
                 }
