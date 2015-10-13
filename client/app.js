324642132fb20fc0dc7ed623ab986a45c16adf72
zhenzhongxu <flyfree@gmail.com>
2015-10-12 22:28:02 -0700

    fix project structure

--- client/app.js
+++ client/app.js
@@ -1,15 +1,39 @@
 if (Meteor.isClient) {
-  angular.module('socially', ['angular-meteor']);
- 
-  angular.module('socially').controller('PartiesListCtrl', ['$scope', function ($scope) {
-    $scope.tasks = [
-        { text: 'This is task 1' },
-        { text: 'This is task 2' },
-        { text: 'This is task 3' }
-      ];
+
+	angular.module('app', ['angular-meteor']);
+
+	angular.module('app').controller('appCtrl', ['$scope', function ($scope){
+	    $scope.items = [{type:"volunteer",
+	                        pic:"",
+	                        title:"Save cheerleader, save the world",
+	                        organization:"Avenger",
+	                        timeScope:"1  year",
+	                        place:"USA"},
+	                        {type:"volunteer",
+	                        pic:"",
+	                        title:"Save cheerleader, save the world",
+	                        organization:"Avenger",
+	                        timeScope:"1  year",
+	                        place:"China"},
+	                        {type:"volunteer",
+	                        pic:"",
+	                        title:"Save cheerleader, save the world",
+	                        organization:"Avenger",
+	                        timeScope:"1  year",
+	                        place:"OOOO"},
+	                        {type:"volunteer",
+	                        pic:"",
+	                        title:"Save cheerleader, save the world",
+	                        organization:"Avenger",
+	                        timeScope:"1  year",
+	                        place:"OOOO"}];
+
+	        $scope.showPopover = function() {
+	                $scope.popoverIsVisible = true;
+	        };
+
+	        $scope.hidePopover = function () {
+	                $scope.popoverIsVisible = false;
+	        };
   }]);
 }
-
-
-
-
